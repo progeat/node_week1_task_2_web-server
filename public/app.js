44b0234a193cee4cc0a6e052f62c7af7d45d11af
Khokhlov Andrey <progeat@mail.ru>
2024-10-30 13:28:30 +0300

    Add method PUT

---
+++ public/app.js
@@ -0,0 +1,37 @@
+document.addEventListener('click', ({ target }) => {
+  if (target.dataset.type === 'remove') {
+    const id = target.dataset.id;
+
+    remove(id).then(() => {
+      target.closest('li').remove();
+    });
+  }
+
+  if (target.dataset.type === 'edit') {
+    const id = target.dataset.id;
+    const itemEdit = target.closest('.list-group-item');
+    const elementText = itemEdit.querySelector('.item-text');
+    const currentText = elementText.textContent;
+    const editedText = prompt('Введите новое значение', currentText).trim();
+
+    if (editedText) {
+      updated(id, editedText).then(() => {
+        elementText.textContent = editedText;
+      });
+    }
+  }
+});
+
+async function updated(id, title) {
+  await fetch(`/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({ title }),
+  });
+}
+
+async function remove(id) {
+  await fetch(`/${id}`, { method: 'DELETE' });
+}
